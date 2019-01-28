d100735860d8562971ad32fc7355ebbe6ad4de16
Adam Shelton <aqs@uchicago.edu>
2019-01-27 21:16:32 -0600

    Tweaked old charts, added new charts, added an index.html with some charts and explanations

--- get_census_data.R
+++ get_census_data.R
@@ -9,4 +9,25 @@ source("api_keys.R")
 census_api_key(census_key)
 
 acs_vars_17 = load_variables(year = 2017, dataset = "acs5")
-acs_17 = get_acs(geography = "zcta", year = 2017, variables = c(total_pop = "B00001_001", white = "C02003_003", aggr_fam_inc = "B19127_001", inet_w_sub = "B28002_002", dial_up_only = "B28002_003", broadband_any = "B28002_004", cell_inet = "B28002_005", cell_inet_only = "B28002_006", broadband_wired = "B28002_007", broadband_wired_only = "B28002_008", satelite = "B28002_009", satelite_only = "B28002_010", other_inet = "B28002_011", inet_wo_sub = "B28002_012", no_inet = "B28002_013" ))
+acs_17 = get_acs(geography = "zcta", year = 2017, geometry = TRUE, moe_level = 95,
+                 variables = c(total_pop = "B01003_001", white = "B02001_002", 
+                               med_age = "B01002_001", med_income = "B19326_001", 
+                               female = "B01001_026", aggr_fam_inc = "B19127_001", 
+                               below_poverty ="B10059_002", employed = "B27011_003", 
+                               commute_time_car = "B08136_002", commute_time_pub_trans = "B08136_007", 
+                               commute_time_walk = "B08136_011", commute_time_other = "B08136_012", 
+                               month_housing_costs = "B25105_001", work_outside_res_area = "B08008_003", 
+                               inet_w_sub = "B28002_002", dial_up_only = "B28002_003", 
+                               broadband_any = "B28002_004", cell_inet = "B28002_005", 
+                               cell_inet_only = "B28002_006", broadband_wired = "B28002_007", 
+                               broadband_wired_only = "B28002_008", satelite = "B28002_009", 
+                               satelite_only = "B28002_010", other_inet = "B28002_011", 
+                               inet_wo_sub = "B28002_012", no_inet = "B28002_013", 
+                               desktop_alone = "B28001_004", smartphone_alone = "B28001_006", 
+                               tablet_alone = "B28001_008", other_comp_alone = "B28001_010" ))
+acs_17_vals = acs_17 %>% select(-one_of("moe")) %>% spread(key = variable, value = estimate)
+acs_17_moes = acs_17 %>% select(-one_of("estimate")) %>% spread(key = variable, value = moe)
+saveRDS(acs_vars_17, file = "acs_vars_17.rds")
+saveRDS(acs_17, file = "acs_17.rds")
+saveRDS(acs_17_vals, file = "acs_17_vals.rds")
+saveRDS(acs_17_moes, file = "acs17_moes.rds")
