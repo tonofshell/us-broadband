9ade3ea50805adb6fc6284543e1488b27b115d82
Adam Shelton <aqs@uchicago.edu>
2019-02-20 18:43:42 -0600

    Added modified data as JSON, started building plot

---
+++ export_d3_data.R
@@ -0,0 +1,11 @@
+# Export Data for D3
+library(readr)
+library(jsonlite)
+library(tidyverse)
+library(here)
+urban_bb = read_csv2(here("Datasets", "US_Urban_Rate_Broadband_Survey.csv"))
+
+d3_test = tibble(dload_speed = as.numeric(urban_bb$`Download Bandwidth Mbps`), 
+                 total_charge = as.numeric(urban_bb$`Total Charge`),
+                 tech = urban_bb$Technology)
+d3_test %>% toJSON() %>% write_lines(here("D3", "urban_bb_sub.json"))
