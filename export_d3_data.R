5c85cb5e31d1368c7055ca81bddc1f4f076b9368
Adam Shelton <aqs@uchicago.edu>
2019-02-26 09:40:56 -0600

    Finished First D3 Chart

--- export_d3_data.R
+++ export_d3_data.R
@@ -9,3 +9,5 @@ d3_test = tibble(dload_speed = as.numeric(urban_bb$`Download Bandwidth Mbps`),
                  total_charge = as.numeric(urban_bb$`Total Charge`),
                  tech = urban_bb$Technology)
 d3_test %>% toJSON() %>% write_lines(here("D3", "urban_bb_sub.json"))
+
+d3_test[1:200,] %>% toJSON() %>% write_lines(here("D3", "urban_bb_small.json"))
