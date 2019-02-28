9cdfcaaa10f8856ef420a30129d070e7dbf32175
Adam Shelton <aqs@uchicago.edu>
2019-02-28 11:28:14 -0600

    Testing GeoJSON data

--- export_d3_data.R
+++ export_d3_data.R
@@ -7,6 +7,7 @@ library(here)
 library(sf)
 library(mapdata)
 library(readxl)
+library(stringr)
 
 urban_bb = read_csv2(here("Datasets", "US_Urban_Rate_Broadband_Survey.csv"))
 
@@ -18,16 +19,16 @@ d3_test %>% toJSON() %>% write_lines(here("D3", "urban_bb_sub.json"))
 d3_test[1:200,] %>% toJSON() %>% write_lines(here("D3", "urban_bb_small.json"))
 
 
-us_sf = st_as_sf(map("county", plot = FALSE, fill = TRUE)) %>% separate(ID, c("state", "subregion"), sep = ",")
-
 land_area = read_excel(here("Datasets", "LND01.xls")) %>% select(STCOU, LND110210D)
+# Oglala Lakota County, SD was Shannon County before 2015, which needs to be corrected, as apparently this area database ("LND01.xls") has not been updated since the name change
 land_area[land_area$STCOU == 46113,]$STCOU = 46102
-acs_17 = land_area %>% rename(GEOID = STCOU, land_area = LND110210D) %>% right_join(readRDS(here("Datasets", "acs_17_vals.rds")))
+acs_17 = land_area %>% rename(GEOID = STCOU, land_area = LND110210D) %>% right_join(readRDS(here("Datasets", "acs_17_vals.rds"))) %>% separate(NAME, c("subregion", "state"), sep = ", ") %>% filter(state != "Puerto Rico")
 
-%>% inner_join(as_tibble(us_sf)[,c(4,16,17)], by = "GEOID")
-
-acs_17_mod = acs_17[1:length(acs_17[[1]]) ,c(1,3,21,22,23,31,34,35,36)] %>%
+# vector of indices to not normalize in some way
+unmodified_vars = c(1, 2, 3, 4, 22, 23, 24, 32, 35)
+acs_17_mod = acs_17[1:length(acs_17[[1]]) ,unmodified_vars] %>%
   bind_cols(data.frame(pop_dens = (acs_17$total_pop / acs_17[,2]))) %>% 
-  bind_cols(as_tibble(acs_17[, -c(1,2,3,21,22,23,31,34,35,36)] / acs_17$total_pop)) %>% 
-  rename("pop_dens" = land_area, "avg_fam_inc" = aggr_fam_inc) %>%
-  separate(NAME, c("county", "state"), sep = ", ")
+  bind_cols(as_tibble(acs_17[, -unmodified_vars] / acs_17$total_pop)) %>% 
+  rename("pop_dens" = land_area1, "avg_fam_inc" = aggr_fam_inc)
+
+acs_17_mod %>% toJSON() %>% write_lines(here("D3", "acs_17_mod.json"))
